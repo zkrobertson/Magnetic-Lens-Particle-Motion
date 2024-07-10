61cbda5b9e8376cb8d6e386b0d2c08ce2e5272a6
Zach <robzach@rams.colostate.edu>
2024-07-10 12:16:11 -0600

    gyro radius

--- py/gyro_radius.py
+++ py/gyro_radius.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 if __name__ == "__main__":
-    r = 3 * 2.54 / 100
-    eV = 400
+    B = 20e-4
+    eV = 1
     q = 1.602e-19
     mass = np.arange(1, 20, 1) * 1.67e-27
-    mass = 32 * 1.67e-27
 
     velocity = np.sqrt(2 * q * eV / mass)
-    B = mass * velocity / r / q * 10000
+    r = mass * velocity / B / q * 10000
 
-    print("B-Field: ", B)
+    print("Gyro Radius: ", r)
