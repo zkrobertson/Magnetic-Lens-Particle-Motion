ea30dc03caab1dbeb892d8cb74852ac5a55eee74
Zach <robzach@rams.colostate.edu>
2024-09-05 11:09:32 -0600

    Gyro Radius Python File modifications

--- py/gyro_radius.py
+++ py/gyro_radius.py
@@ -1,15 +1,37 @@
 import numpy as np
 
+# Must provide these two
+charge = 1  # -1 for electron, 1 for ion, 2 for double ion...
+mass = 18  # AMU
+
+# Provide all but one
+# One can be a numpy.ndarray
+energy = np.arange(100, 2400, 100)  # eV
+B = None  # Gauss
+r = 0.12  # meters
+
 if __name__ == "__main__":
-    charge = [1.602e-19]
-    mass = [18 * 1.67e-27]
+    charge = charge * 1.602e-19
+    mass = mass * 1.66e-27
 
-    eV = 2400
-    r = 0.06
+    if B is not None:
+        B = B * 1e-4
 
-    velocity = [np.sqrt(2 * q * eV / m) for q, m in zip(charge, mass)]
+    if r is None:
+        velocity = np.sqrt(2 * charge * energy / mass)
+        r = velocity * mass / B / charge
+    elif energy is None:
+        velocity = B * r * charge / mass
+        energy = 1 / 2 * mass / charge * np.power(velocity, 2)
+    elif B is None:
+        velocity = np.sqrt(2 * charge * energy / mass)
+        B = velocity * mass / r / charge
 
-    B = [10000 * v * m / q / r for v, q, m in zip(velocity, charge, mass)]
-    print(B)
+    mass = mass * 6.022e26
+    charge = charge / 1.602e-19
+    B = B * 1e4
 
-    # eV = velocity**2 * mass / q / 2
+    print(f"For an particle with Mass {mass} AMU and charge {charge}")
+    print(f"\tMagnetic Field: {B} Gauss")
+    print(f"\tEnergy: {energy} eV")
+    print(f"\tGyro Radius: {r} meters")
